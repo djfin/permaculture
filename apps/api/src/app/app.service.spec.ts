1d5153eadb8c1863b50d2e22763e112d8f8c1235
djfin <djfinley98@gmail.com>
2019-12-21 09:54:54 -0700

    Final MSSE663 commit: Readme, testing try

--- apps/api/src/app/app.service.spec.ts
+++ apps/api/src/app/app.service.spec.ts
@@ -2,15 +2,35 @@ import { Test } from '@nestjs/testing';
 
 import { AppService } from './app.service';
 import{ Project, GardenBed, User, Zone } from '@permaculture/data';
+import { INestApplication } from '@nestjs/common';
+import { UserInt } from 'libs/data/src/lib/data.interface';
+
+const mockAppService = {
+  getUserById: ()=>{
+    const user = new User('','','','','');
+    return Promise.resolve(user);
+  }
+}
 
 describe('AppService', () => {
   let service: AppService;
+  let app: INestApplication
+  const user = new User('testUser', 'password', 'domain@example.com', 'name', 'name');
+  let userService = {findAll: ()=>Array<UserInt>()}
 
   beforeAll(async () => {
-    const app = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       providers: [AppService],
     }).compile();
 
-    service = app.get<AppService>(AppService);
+    service = module.get<AppService>(AppService);
   });
+
+  it('Should get all Users', ()=>{
+    
+  })
+  it('Should get a user', ()=>{
+    
+
+  })
 });
